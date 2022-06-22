8b0b651593b8d92a5303b8d6e5dcb58082c1eb8d
Mikhail V Volkov <74855766+mikhailVvolkov@users.noreply.github.com>
2022-06-22 09:59:24 +0200

    Delete energy_to_field.m

--- energy_to_field.m
+++
@@ -1,5 +0,0 @@
-function F = energy_to_field(E,tau,w0)
-    P = E/sqrt(pi/2)/tau; % where tau is HWe-1M in field
-    I =  2*P/(pi*w0.^2); % where w0 is HWe-2M in intensity, that is HWe-1M in field.
-    F = sqrt(2*I/cn.c/cn.epsilon_0); 
-end
